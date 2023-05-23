2162cf5a362a16e618319d267984ddd7d4c533ad
JoelBookey <joelbookey@gmail.com>
2023-05-23 21:24:37 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,111 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() -> Result<(), String> {
-    // read in file
+    // collect command line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         return Err("please provide a file".to_string());
     }
+
+    // read file from arguments
     let file_path = &args[1];
-    let contents = match read_in_file(&file_path) {
+    let contents = match read_in_file(file_path) {
         Ok(contents) => contents,
         Err(error) => {
             return Err(error.to_string());
         }
     };
 
-    // lexering
-    let mut keys: HashMap<char, Instruction> = HashMap::new();
-    keys.insert('<', Instruction::Left);
-    keys.insert('>', Instruction::Right);
-    keys.insert('+', Instruction::Plus);
-    keys.insert('-', Instruction::Minus);
-    keys.insert('[', Instruction::Begin);
-    keys.insert(']', Instruction::End);
-    keys.insert('.', Instruction::Print);
-    keys.insert(',', Instruction::Input);
-
-    let instructions = contents
+    // do some lexering into a vector (oh yeahhhhh)
+    let tokens = contents
+        .trim()
         .chars()
-        .filter_map(|x| keys.get(&x))
-        .collect::<Vec<&Instruction>>();
+        .filter_map(|x| match x {
+            '<' => Some(Token::Left),
+            '>' => Some(Token::Right),
+            '+' => Some(Token::Plus),
+            '-' => Some(Token::Minus),
+            '[' => Some(Token::Begin),
+            ']' => Some(Token::End),
+            '.' => Some(Token::Print),
+            ',' => Some(Token::Input),
+            _ => None,
+        })
+        .collect::<Vec<Token>>();
 
-    do_your_thing(instructions)?;
+    do_your_thing(tokens)?;
 
     Ok(())
 }
 
-fn do_your_thing(instructions: Vec<&Instruction>) -> Result<(), &'static str> {
-    let mut big_array: [u8; 30000] = [0; 30000];
+fn do_your_thing(tokens: Vec<Token>) -> Result<(), String> {
+    let mut tape: [u8; 30000] = [0; 30000];
     let mut pointer: usize = 0;
-    let mut instruction_number = 0;
+    let mut token_number = 0;
     let mut loop_indexes: Vec<usize> = Vec::new();
-    while instruction_number < instructions.len() {
-        //println!("BEGIN\n{:?}{:?}", &big_array.last(), &big_array[0..70]);
-        //dbg!(instructions.get(instruction_number).unwrap());
-        //println!("{}, END\n", pointer);
-        if instructions.get(instruction_number).unwrap() == &&Instruction::Left {
+
+    while token_number < tokens.len() {
+        if tokens.get(token_number).unwrap() == &Token::Left {
             if pointer == 0 {
-                pointer = 30000 - 1;
+                pointer = 29999;
             } else {
                 pointer -= 1;
             }
-            instruction_number += 1;
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Right {
+            token_number += 1;
+        } else if tokens.get(token_number).unwrap() == &Token::Right {
             if pointer == 30000 - 1 {
                 pointer = 0;
             } else {
                 pointer += 1;
             }
-            instruction_number += 1;
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Plus {
-            if big_array[pointer] == 255 {
-                big_array[pointer] = 0;
+            token_number += 1;
+        } else if tokens.get(token_number).unwrap() == &Token::Plus {
+            tape[pointer] = tape[pointer].wrapping_add(1);
+            token_number += 1;
+        } else if tokens.get(token_number).unwrap() == &Token::Minus {
+            tape[pointer] = tape[pointer].wrapping_sub(1);
+            token_number += 1;
+        } else if tokens.get(token_number).unwrap() == &Token::Begin {
+            if tape[pointer] != 0 {
+                loop_indexes.push(token_number);
+                token_number += 1;
             } else {
-                big_array[pointer] += 1;
-            }
-            instruction_number += 1;
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Minus {
-            if big_array[pointer] == 0 {
-                big_array[pointer] = 255;
-            } else {
-                big_array[pointer] -= 1;
-            }
-            instruction_number += 1;
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Begin {
-            if big_array[pointer] != 0 {
-                loop_indexes.push(instruction_number as usize);
-                instruction_number += 1;
-            } else {
-                let mut loop_number = instruction_number.clone() + 1;
+                let mut loop_number = token_number + 1;
                 let mut offset = 0;
-                while loop_number < instructions.len() {
-                    if instructions.get(loop_number).unwrap() == &&Instruction::End && offset == 0 {
-                        instruction_number = loop_number + 1;
+                while loop_number < tokens.len() {
+                    if tokens.get(loop_number).unwrap() == &Token::End && offset == 0 {
+                        token_number = loop_number + 1;
                         break;
-                    } else if instructions.get(loop_number).unwrap() == &&Instruction::Begin {
+                    } else if tokens.get(loop_number).unwrap() == &Token::Begin {
                         offset -= 1;
-                    } else if instructions.get(loop_number).unwrap() == &&Instruction::End {
+                    } else if tokens.get(loop_number).unwrap() == &Token::End {
                         offset += 1;
                     }
                     loop_number += 1;
                 }
             }
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::End {
-            if big_array[pointer] != 0 {
-                instruction_number = *loop_indexes.last().unwrap() + 1;
+        } else if tokens.get(token_number).unwrap() == &Token::End {
+            if tape[pointer] != 0 {
+                token_number = *loop_indexes.last().unwrap() + 1;
             } else {
                 loop_indexes.pop();
-                instruction_number += 1;
+                token_number += 1;
             }
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Print {
-            print!("{}", big_array[pointer] as char);
-            instruction_number += 1;
-        } else if instructions.get(instruction_number).unwrap() == &&Instruction::Input {
-            let input: i32 = std::io::stdin()
-                .bytes()
-                .next()
-                .and_then(|result| result.ok())
-                .map(|byte| byte as i32)
-                .unwrap();
-            big_array[pointer] = input as u8;
-            instruction_number += 1;
+        } else if tokens.get(token_number).unwrap() == &Token::Print {
+            token_number += 1;
+            print!("{}", tape[pointer] as char);
+        } else if tokens.get(token_number).unwrap() == &Token::Input {
+            let input = std::io::stdin().bytes().next().unwrap().unwrap();
+            tape[pointer] = input;
+            token_number += 1;
         } else {
-            return Err("instruction not handled");
+            return Err("token not handled".into());
         }
     }
+
     println!(" ");
     Ok(())
 }
@@ -132,7 +120,7 @@ fn read_in_file(file_path: &String) -> Result<String, std::io::Error> {
 }
 
 #[derive(Debug, PartialEq)]
-enum Instruction {
+enum Token {
     Left,
     Right,
     Plus,
